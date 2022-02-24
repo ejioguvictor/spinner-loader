ec42f09cc94a2bbad87565de99f1f620431d9633
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-24 21:58:58 +0100

    spinner-loader animation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="loader">
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
