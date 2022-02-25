5f3801fdff79f9e2289b3734f6581c2c6447c024
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-25 11:43:52 +0100

    modified spinner

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className={styles.App}>
       <div className={styles.container}>
+        <h3 className={styles.spinner}>Login successful! Redirecting...</h3>
         <div className={styles.loader}>
           <span></span>
         </div>
